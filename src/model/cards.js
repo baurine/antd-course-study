f25a1967a7bbef8c455c61cc4d01e3b870598286
baurine <2008.hbl@gmail.com>
2018-10-04 20:58:16 +0800

    4.1 - Table list

---
+++ src/model/cards.js
@@ -0,0 +1,45 @@
+const delay = (millisecond) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecond)
+  })
+}
+
+export default {
+  namespace: 'cards',
+
+  state: {
+    cardsList: []
+  },
+
+  effects: {
+    *queryList(_, sagaEffects) {
+      const listData = [
+        {
+          name: 'umi',
+          desc: '极快的类 Next.js 的 React 应用框架',
+          url : 'https://umijs.org'
+        },
+        {
+          name: 'antd',
+          desc: '一个服务企业级的设计体系',
+          url : 'https://ant.design/index-cn'
+        },
+        {
+          name: 'antd-pro',
+          desc: '一个服务企业级的设计体系',
+          url : 'https://ant.design/index-cn'
+        }
+      ]
+      const { call, put } = sagaEffects
+      yield call(delay, 3000)
+      yield put({type: 'initList', payload: listData})
+    }
+  },
+
+  reducers: {
+    initList(state, {payload}) {
+      const cardsList = [...payload]
+      return {cardsList}
+    }
+  }
+}
--- src/page/List/index.js
+++ src/page/List/index.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { Table } from 'antd'
+import { connect } from 'dva'
 
-export default class List extends React.Component {
+function mapStateToProps(state) {
+  return {
+    cardsList: state.cards.cardsList,
+    cardsLoading: state.loading.effects['cards/queryList']
+  }
+}
+
+class List extends React.Component {
+  columns = [
+    {
+      title: '名称',
+      dataIndex: 'name'
+    },
+    {
+      title: '描述',
+      dataIndex: 'desc'
+    },
+    {
+      title: '链接',
+      dataIndex: 'url',
+      render: value => <a href={value}>{value}</a>
+    }
+  ]
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'cards/queryList'
+    })
+  }
+
   render() {
+    const { cardsList, cardsLoading } = this.props
     return (
       <div>
-        hello world
+        <Table
+          columns={this.columns}
+          dataSource={cardsList}
+          loading={cardsLoading}
+          rowKey="name"/>
       </div>
     )
   }
 }
+
+export default connect(mapStateToProps)(List)
