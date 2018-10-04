508d8b0125f0290e4773a95dc9923c96b23ba7fa
baurine <2008.hbl@gmail.com>
2018-10-04 14:27:47 +0800

    3.1 - Model by dva

--- src/page/PuzzleCards.js
+++ src/page/PuzzleCards.js
@@ -1,44 +1,45 @@
 import { Component } from "react";
 import { Card, Button } from 'antd';
+import { connect } from 'dva';
 
+const namespace = 'puzzlecards'
+const mapStateToProps = (state) => {
+  const cardList = state[namespace].data
+  return {cardList}
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClickAdd: (newCard) => {
+      const action = {
+        type: `${namespace}/addNewCard`,
+        payload: newCard
+      }
+      dispatch(action)
+    }
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class PuzzleCardsPage extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      cardList: [
-        {
-          id: 1,
-          setup: 'Did you hear about the two silk worms in a race?',
-          punchline: 'It ended in a tie',
-        },
-        {
-          id: 2,
-          setup: 'What happens to a frog\'s car when it breaks down?',
-          punchline: 'It gets toad away'
-        }
-      ]
-    }
     this.counter = 100
   }
 
   addNewCard = () => {
-    this.setState(prevState => ({
-      cardList: [
-        ...prevState.cardList,
-        {
-          id: this.counter++,
-          setup: 'Lorem ipsum...',
-          punchline: 'sed do eiusmod...'
-        }
-      ]
-    }))
+    this.props.onClickAdd(
+      {
+        setup: 'Lorem ipsum...',
+        punchline: 'here we use dva...'
+      }
+    )
   }
 
   render() {
     return (
       <div>
         {
-          this.state.cardList.map(card => 
+          this.props.cardList.map(card => 
             <Card key={card.id}>
               <div>Q: {card.setup}</div>
               <div>
