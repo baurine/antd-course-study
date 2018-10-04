def068e92555da5cd08723ae6e0916a57538b2e6
baurine <2008.hbl@gmail.com>
2018-10-04 11:51:48 +0800

    2.3 - Menu and routes

--- config/config.js
+++ config/config.js
@@ -13,8 +13,20 @@ export default {
       component: '../layout',
       routes: [
         {
-          path: 'helloworld',
-          component: './HelloWorld'
+          path: '/',
+          component: 'HelloWorld'
+        },
+        {
+          path: '/helloworld',
+          component: 'HelloWorld'
+        },
+        {
+          path: '/dashboard',
+          routes: [
+            { path: '/dashboard/analysis',  component: 'Dashboard/Analysis' },
+            { path: '/dashboard/monitor',   component: 'Dashboard/Monitor' },
+            { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
+          ]
         }
       ]
     }
