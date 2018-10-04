5ee5a5626b331575a85ffdf46b47f96627b1304e
baurine <2008.hbl@gmail.com>
2018-10-04 11:29:30 +0800

    2.1 - Use Layout / Header / Footer / Sider / Content

--- config/config.js
+++ config/config.js
@@ -10,7 +10,13 @@ export default {
   routes: [
     {
       path: '/',
-      component: './HelloWorld'
+      component: '../layout',
+      routes: [
+        {
+          path: 'helloworld',
+          component: './HelloWorld'
+        }
+      ]
     }
   ]
 }
